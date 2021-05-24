e67cd7a41ff88dd050c09ec7a77092c6a656cd6c
guzilla <gusrosales7@gmail.com>
2021-05-24 13:02:38 -0700

    Update loading of models using require

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 "use strict"
-
-const fs = require("fs")
-const path = require("path")
+let modules = [require("./note"), require("./user")]
 const Sequelize = require("sequelize")
-const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || "development"
 const config = require(__dirname + "/../config/config.js")[env]
 const db = {}
@@ -22,16 +19,10 @@ if (config.use_env_variable) {
 	)
 }
 
-fs.readdirSync(__dirname)
-	.filter(file => {
-		return (
-			file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-		)
-	})
-	.forEach(file => {
-		const model = sequelize["import"](path.join(__dirname, file))
-		db[model.name] = model
-	})
+modules.forEach(module => {
+	const model = module(sequelize, Sequelize, config)
+	db[model.name] = model
+})
 
 Object.keys(db).forEach(modelName => {
 	if (db[modelName].associate) {
