18d009f074d5939070b69dbe69595e30979eb678
guzilla <gusrosales7@gmail.com>
2021-05-29 16:00:29 -0700

    Changed finding user notes to only loggedin user

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,12 @@
 const db = require(`../models`)
 
 exports.postNotes = function (req, res) {
+	const loggedUserID = req.user && req.user.id
+
 	// POST route for saving a new note
-	db.Note.findOne({ where: { datestamp: req.body.datestamp } })
+	db.Note.findOne({
+		where: { datestamp: req.body.datestamp, UserId: loggedUserID },
+	})
 		.then(response => {
 			// if the conditional above finds a match (i.e. not new record) update instead of create
 			if (response === null) {
@@ -20,25 +24,14 @@ exports.postNotes = function (req, res) {
 		.catch(err => {
 			// Whenever a validation or flag fails, an error is thrown
 			// We can `catch` the error to prevent it from being `thrown`, which could crash our node app
-			// console.log(err)
 			res.json(err)
 		})
 }
-exports.getNotes = function (req, res) {
-	// POST route for saving a new note
-	// console.log(req.body)
-	db.Note.findAll({})
-		.then(response => {
-			res.json(response)
-		})
-		.catch(err => {
-			// Whenever a validation or flag fails, an error is thrown
-			// We can `catch` the error to prevent it from being `thrown`, which could crash our node app
-			res.json(err)
-		})
-}
+
 exports.getUserNotes = function (req, res) {
-	db.Note.findAll({ where: { UserId: req.params.id } })
+	const loggedUserID = req.user && req.user.id
+
+	db.Note.findAll({ where: { UserId: loggedUserID } })
 		.then(response => {
 			res.json(response)
 		})
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,19 +1,10 @@
-import axios from "axios"
-import moment from "moment"
+import "core-js/stable/promise"
+import "regenerator-runtime/runtime"
 
 export default {
 	// NYT Archive Article
-	getArticle: function () {
-		// console.log(process.env)
-		let randYear = Math.floor(Math.random() * (moment().year() - 1851)) + 1851 // articles begin in 1851
-		const queryURI = `https://api.nytimes.com/svc/archive/v1/${randYear}/${moment().month()}.json?api-key=${
-			process.env.NYT_API_KEY
-		}`
-		return axios.get(queryURI)
-	},
-	getUserNotes: function (id) {
-		// console.log(process.env)
-		const queryURI = `/api/notes` + id
-		return axios.get(queryURI)
+
+	getUserNotes: async function () {
+		return await fetch("/api/notes").then(r => r.json())
 	},
 }
