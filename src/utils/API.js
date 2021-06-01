91763fe3d05df2192063eaed15f5b212e225504d
guzilla <gusrosales7@gmail.com>
2021-06-01 11:29:11 -0700

    Added API helpers for posting notes and changing start hour.

--- src/utils/API.js
+++ src/utils/API.js
@@ -7,4 +7,20 @@ export default {
 	getUserNotes: async function () {
 		return await fetch("/api/notes").then(r => r.json())
 	},
+
+	postNote: async function (note) {
+		return await fetch("/api/notes", {
+			method: "POST",
+			body: JSON.stringify(note),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		}).then(r => r.json())
+	},
+
+	postNewStartHour: async function (hour) {
+		return await fetch(`/api/settings${hour}`, {
+			method: "POST",
+		}).then(r => r.json())
+	},
 }
