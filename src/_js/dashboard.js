3b101b751b541567ab93ea273e40f6477c93f980
guzilla <gusrosales7@gmail.com>
2021-06-01 12:18:02 -0700

    Removed jQuery and Moment imports

--- src/_js/dashboard.js
+++ src/_js/dashboard.js
@@ -1,21 +1,25 @@
-// import moment from "moment"
-import $ from "jquery"
+import "../_css/dashboard.css"
 import API from "../utils/API"
 
-const accountContainer = document.querySelector(`#account-settings`)
 const leftSide = document.querySelectorAll(`.left-side`)
-const accountSettingsBtn = document.querySelector(`#button-account`)
-const plannerSettingsBtn = document.querySelector(`#button-planner`)
+const accountSettingsBtn = document.getElementById(`buttonAccount`)
+const plannerSettingsBtn = document.getElementById(`buttonPlanner`)
+const startHourForm = document.getElementById(`startHourForm`)
+const startHourSelection = document.getElementById(`startHour`)
+const userStartTime = startHourSelection.dataset.startHour
 
 init()
 
 function init() {
-	$(accountContainer).data(`userid`)
+	// find all options and save in array
+	const startHourSelectionOptions = [...startHourSelection.children]
+	// filter out the corresponding option to the default "start time"
+	// and set it as selected
+	startHourSelectionOptions.filter(
+		o => Number(o.value) === Number(userStartTime),
+	)[0].selected = true
 
-	API.getUserNotes($(accountContainer).data(`userid`)).then(data => {
-		$(accountContainer).data(`notes`, data.data[0])
-	})
-
+	// add eventListeners to the different "settings tabs"
 	leftSide.forEach(side => {
 		side.addEventListener(`click`, evt => {
 			switch (evt.target.id) {
@@ -39,3 +43,11 @@ function init() {
 		})
 	})
 }
+function handleStartHourFormEvent(e) {
+	e.preventDefault()
+	API.postNewStartHour(startHourSelection.value).then(datum =>
+		console.log(datum),
+	)
+}
+
+startHourForm.addEventListener("submit", handleStartHourFormEvent)
