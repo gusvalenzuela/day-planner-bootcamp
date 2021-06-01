cbac73a406acb91b4e74ef68eeb16c4bd8e7a2d2
guzilla <gusrosales7@gmail.com>
2021-06-01 11:28:12 -0700

    Changed settings control to postStartHour. Removed Moment dependency. Added convert24to12 helper for handlebars

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,46 +1,58 @@
-// const utils = require(`../utils/utils`)
 const db = require(`../models`)
-const moment = require(`moment`)
 
-exports.settings = function (req, res) {
-	res.render("settings", req.user)
+exports.postStartHour = function (req, res) {
+	return db.User.update(
+		{ startTime: Number(req.params.hour) },
+		{ where: { id: req.session.passport.user } },
+	).then(results => {
+		res.json(results)
+	})
 }
 
 exports.pages = function (req, res) {
 	// for pages that need same data, i'm using a referer to render the appropriate page/view
 	let referer = req._parsedUrl.href.slice(1) // saving the referer in the link
-	var activeUser, loggedIn
 	//checking to see if there's a logged in user detected in the html request
-	if (req.session.passport) {
-		activeUser = req.session.passport.user
-		loggedIn = true
-	} else {
-		activeUser = `none`
-		loggedIn = false
-	}
+
 	db.User.findOne({
-		where: { id: activeUser },
+		where: { id: req.session.passport.user },
 		include: [db.Note],
 		raw: false,
-	}).then(response => {
-		if (response.dataValues.password) {
-			response.dataValues.password = "*".repeat(7)
+	}).then(({ dataValues: user }) => {
+		if (user.password) {
+			user.password = "*".repeat(7)
+		}
+
+		const formatOptions = {
+			weekday: "short",
+			month: "long",
+			day: "numeric",
+			hour: "2-digit",
+			// minute: "2-digit",
 		}
 
 		// formatting the way i want handlebars to display notes' times here
 		// storing it in notes.formattedDate
-		const notesArray = response.dataValues.Notes.map(note => {
-			note.dataValues.formattedDate = moment(note.datetime).format(
-				`ddd, MMM D - hhA`,
-			)
+		const notesArray = user.Notes.map(note => {
+			const formattedDate = new Intl.DateTimeFormat(
+				"en-us",
+				formatOptions,
+			).format(note.datetime)
+			note.dataValues.formattedDate = formattedDate
 			return note.dataValues
 		})
 		res.render(referer, {
-			userID: activeUser,
-			// notes: response.dataValues.Notes[0].dataValues,
-			user: response.dataValues,
+			userID: user.id,
+			user,
 			notes: notesArray,
-			loggedIn: loggedIn,
+			loggedIn: req.session.passport.user ? true : false,
+			helpers: {
+				convert24to12: function (num) {
+					return num > 12
+						? `${num - 12} PM`
+						: `${num < 10 ? `0${num}` : num} AM`
+				},
+			},
 		})
 	})
 }
